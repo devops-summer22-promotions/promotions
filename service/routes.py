817cd662b0170ae61437dc507baae82fa68c35e2
Alex Lee <cloudstrife79@gmail.com>
2022-06-22 16:07:52 -0400

    add initial route for creating a promotion

--- service/routes.py
+++ service/routes.py
@@ -18,6 +18,8 @@ from service.models import Promotion, DataValidationError
 # Import Flask application
 from . import app
 
+CONTENT_TYPE_JSON = "application/json"
+
 ######################################################################
 # GET INDEX
 ######################################################################
@@ -29,6 +31,25 @@ def index():
         status.HTTP_200_OK,
     )
 
+######################################################################
+# ADD A NEW PROMOTION
+######################################################################
+@app.route("/promotions", methods=["POST"])
+def create_promo():
+    """
+    Creates a Promotion
+    This endpoint will create a Promotion based the data in the body that is posted
+    """
+    app.logger.info("Request to create a promotion")
+    check_content_type(CONTENT_TYPE_JSON)
+    promo = Promotion()
+    promo.deserialize(request.get_json())
+    promo.create()
+    message = promo.serialize()
+    location_url = url_for("get_promos", promo_id=promo.id, _external=True)
+
+    app.logger.info("Promotion with ID [%s] created.", promo.id)
+    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
@@ -39,3 +60,14 @@ def init_db():
     """ Initializes the SQLAlchemy app """
     global app
     Promotion.init_db(app)
+
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        "Content-Type must be {}".format(media_type),
+    )
