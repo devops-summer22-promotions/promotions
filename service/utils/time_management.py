61216bab99bb83a87df07aad6cfb5b9ff8c16fc9
Alex Lee <cloudstrife79@gmail.com>
2022-06-22 23:14:14 -0400

    add helper function for converting postgres-format date strings to python datetime.date

---
+++ service/utils/time_management.py
@@ -0,0 +1,11 @@
+from datetime import date, datetime
+
+def str_to_dt(date_string):
+    """
+    Converts date_string in Postgres format to a 3-tuple datetime for comparison in tests.
+    """
+    dt = datetime.strptime(date_string, "%a, %d %b %Y %H:%M:%S GMT")
+    year = dt.year
+    month = dt.month
+    day = dt.day
+    return date(year, month, day)
