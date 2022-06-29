dcabb13b24bc3f72a138e34defe116b616b5146f
ldm <liudamingdl@gmail.com>
2022-06-29 14:32:19 -0400

    handle errors if id is not a number or id is out of range in API FIND A PROMOTION BY ID

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -242,3 +242,19 @@ class TestPromotionServer(TestCase):
         )
         self.assertEqual(response.status_code,
                          status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    def test_find_promo_by_id_not_a_number(self):
+        """It should not find a promotion with a non-number id"""
+        response = self.client.get(
+            BASE_URL + '/' + 'a',
+        )
+        self.assertEqual(response.status_code,
+                         status.HTTP_400_BAD_REQUEST)
+
+    def test_find_promo_by_id_out_of_range(self):
+        """It should not find a promotion with a number out of range"""
+        response = self.client.get(
+            BASE_URL + '/' + '2147483648',
+        )
+        self.assertEqual(response.status_code,
+                         status.HTTP_400_BAD_REQUEST)
