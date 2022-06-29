6ad4ef0e6110d6a2c542359165090b4482830058
Alex Lee <cloudstrife79@gmail.com>
2022-06-29 12:50:41 -0400

    update index route to return some useful info

--- service/routes.py
+++ service/routes.py
@@ -29,7 +29,11 @@ CONTENT_TYPE_JSON = "application/json"
 def index():
     """ Root URL response """
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="Promotion REST API Service",
+            version="1.0",
+            paths=url_for("list_promos", _external=True),
+        ),
         status.HTTP_200_OK,
     )
 
