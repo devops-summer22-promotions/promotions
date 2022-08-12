8d07fcde36087b6cb6bfd18a62716cda7be56c31
Alex Lee <cloudstrife79@gmail.com>
2022-08-12 13:43:50 -0400

    add flask-restx model for promotion collection path; fix url_for calls to use proper API

--- service/routes.py
+++ service/routes.py
@@ -67,7 +67,7 @@ promotion_model = api.inherit(
     'PromotionModel',
     create_model,
     {
-        '_id': fields.String(readOnly=True, # change to 'id' if this doesn't work; '_id' might be a couchdb thing
+        'id': fields.String(readOnly=True, # change to 'id' if this doesn't work; '_id' might be a couchdb thing
                              description='The unique ID assigned internally by the service')
     }
 )
@@ -81,6 +81,10 @@ promotion_model = api.inherit(
 promotion_args = reqparse.RequestParser()
 promotion_args.add_argument('name', type=str, required=False, help='List Promotions by name')
 promotion_args.add_argument('type', type=str, required=False, help='List Promotions by type')
+promotion_args.add_argument('discount', type=int, required=False, help='List Promotions by percent discount')
+promotion_args.add_argument('customer', type=int, required=False, help='List Promotions by associated customer ID')
+promotion_args.add_argument('start_date', type=str, required=False, help='List Promotions by start date')
+promotion_args.add_argument('end_date', type=str, required=False, help='List Promotions by end date')
 
 
 ######################################################################
@@ -119,7 +123,7 @@ class PromotionResource(Resource):
 
         else:
             message = promo.serialize()
-            location_url = url_for("find_promo", promo_id=promo.id, _external=True)
+            location_url = api.url_for(PromotionResource, promo_id=promo.id, _external=True)
             app.logger.info("Promotion with ID [%s] found.", promo.id)
             return jsonify(message), status.HTTP_200_OK, {"Location": location_url}
 
@@ -169,6 +173,118 @@ class PromotionResource(Resource):
         return '', status.HTTP_204_NO_CONTENT
 
 
+######################################################################
+#  PATH: /promotions
+######################################################################
+@api.route('/promotions', strict_slashes=False)
+class PromotionCollection(Resource):
+    """ Handles all interactions with collections of Promotions """
+    #------------------------------------------------------------------
+    # LIST ALL PROMOTIONS
+    #------------------------------------------------------------------
+    @api.doc('list_promotions')
+    @api.expect(promotion_args, validate=True)
+    @api.marshal_list_with(promotion_model)
+    def get(self):
+        """ Returns all of the Promotions """
+        app.logger.info('Request to list Promotions...')
+        promotions = []
+        args = promotion_args.parse_args()
+        promotions = Promotion.all()
+
+        if args['type']:
+            query_type = args['type']
+            app.logger.info("type = %s", query_type)
+            promotions = filter(lambda x: (
+                query_type == str(x.type.name)), promotions)
+
+        if args['name']:
+            query_name = args['name']
+            app.logger.info("name contains %s", query_name)
+            promotions = filter(lambda x: (query_name in x.name), promotions)
+
+        if args['discount']:
+            query_discount = args['discount']
+            app.logger.info("discount = %s", query_discount)
+            promotions = filter(lambda x: (
+                int(query_discount) == x.discount), promotions)
+
+        if args['customer']:
+            query_customer = args['customer']
+            app.logger.info("customer = %s", query_customer)
+            promotions = filter(lambda x: (
+                int(query_customer) == x.customer), promotions)
+
+        if args['start_date']:
+            query_start_date = args['start_date']
+            app.logger.info("start_date = %s", query_start_date)
+            promotions = filter(lambda x: query_start_date ==
+                                '{:%Y-%m-%d}'.format(x.start_date), promotions)
+
+        if args['end_date']:
+            query_end_date = args['end_date']
+            app.logger.info("end_date = %s", query_end_date)
+            promotions = filter(lambda x: query_end_date ==
+                                '{:%Y-%m-%d}'.format(x.end_date), promotions)
+
+        results = [promo.serialize() for promo in promotions]
+        app.logger.info("Returning %d promotions", len(results))
+        return results, status.HTTP_200_OK
+
+
+    #------------------------------------------------------------------
+    # ADD A NEW PROMOTION
+    #------------------------------------------------------------------
+    @api.doc('create_promotion')
+    @api.response(400, 'The posted data was not valid')
+    @api.expect(create_model)
+    @api.marshal_with(promotion_model, code=201)
+    def post(self):
+        """
+        Creates a Pet
+        This endpoint will create a Pet based the data in the body that is posted
+        """
+        app.logger.info("Request to create a Promotion")
+        promo = Promotion()
+        app.logger.debug('Payload = %s', api.payload)
+        promo.deserialize(api.payload)
+        # check to see if this is a duplicate
+        named_promos = Promotion.find_by_name(promo.name)
+        if (named_promos != []):
+            for other_promo in named_promos:
+                if check_duplicate(promo, other_promo):
+                    abort(status.HTTP_409_CONFLICT,
+                        "Attempt to create duplicate Promotion")
+        promo.create()
+        message = promo.serialize()
+        location_url = api.url_for(PromotionResource, promo_id=promo.id, _external=True)
+
+        app.logger.info("Promotion with ID [%s] created.", promo.id)
+        return promo.serialize(), status.HTTP_201_CREATED, {'Location': location_url}
+
+    #------------------------------------------------------------------
+    # DELETE ALL PETS (for testing only)
+    #------------------------------------------------------------------
+    @api.doc('delete_all_promotions')
+    @api.response(204, 'All Promotions deleted')
+    def delete(self):
+        """
+        Delete all Promotions
+
+        This endpoint will delete all Promotions only if the system is under test
+        """
+        app.logger.info('Request to Delete all promotions...')
+        # NO NEED FOR THIS IN HOMEWORK - CAN JUST CLEAR THE DATABASE TABLE
+        if "TESTING" in app.config and app.config["TESTING"]:
+            Promotion.remove_all()
+            app.logger.info("Removed all Pets from the database")
+        else:
+            app.logger.warning("Request to clear database while system not under test")
+
+        return '', status.HTTP_204_NO_CONTENT
+
+
+
 
 
 
@@ -176,48 +292,48 @@ class PromotionResource(Resource):
 # ORIGINAL CODE BELOW #
 #######################
 
-######################################################################
-# ADD A NEW PROMOTION
-######################################################################
+# ######################################################################
+# # ADD A NEW PROMOTION
+# ######################################################################
 
 
-@app.route("/promotions", methods=["POST"])
-def create_promo():
-    """
-    Creates a Promotion
-    This endpoint will create a Promotion based the data in the body that is posted
-    """
-    app.logger.info("Request to create a promotion")
-    check_content_type(CONTENT_TYPE_JSON)
-    promo = Promotion()
-    promo.deserialize(request.get_json())
+# @app.route("/promotions", methods=["POST"])
+# def create_promo():
+#     """
+#     Creates a Promotion
+#     This endpoint will create a Promotion based the data in the body that is posted
+#     """
+#     app.logger.info("Request to create a promotion")
+#     check_content_type(CONTENT_TYPE_JSON)
+#     promo = Promotion()
+#     promo.deserialize(request.get_json())
 
-    # check whether customer id is an integer
-    if promo.customer == "":
-        promo.customer = None
+#     # check whether customer id is an integer
+#     if promo.customer == "":
+#         promo.customer = None
 
-    if promo.customer != None:
-        try:
-            id = int(promo.customer)
-        except:
-            app.logger.info(
-                "Customer ID [%s] is not a number.", promo.customer)
-            abort(status.HTTP_400_BAD_REQUEST,
-                  f"Customer ID {promo.customer} should be a number.")
+#     if promo.customer != None:
+#         try:
+#             id = int(promo.customer)
+#         except:
+#             app.logger.info(
+#                 "Customer ID [%s] is not a number.", promo.customer)
+#             abort(status.HTTP_400_BAD_REQUEST,
+#                   f"Customer ID {promo.customer} should be a number.")
 
-    # check to see if this is a duplicate
-    named_promos = Promotion.find_by_name(promo.name)
-    if (named_promos != []):
-        for other_promo in named_promos:
-            if check_duplicate(promo, other_promo):
-                abort(status.HTTP_409_CONFLICT,
-                      "Attempt to create duplicate Promotion")
-    promo.create()
-    message = promo.serialize()
-    location_url = url_for("create_promo", promo_id=promo.id, _external=True)
+#     # check to see if this is a duplicate
+#     named_promos = Promotion.find_by_name(promo.name)
+#     if (named_promos != []):
+#         for other_promo in named_promos:
+#             if check_duplicate(promo, other_promo):
+#                 abort(status.HTTP_409_CONFLICT,
+#                       "Attempt to create duplicate Promotion")
+#     promo.create()
+#     message = promo.serialize()
+#     location_url = url_for("create_promo", promo_id=promo.id, _external=True)
 
-    app.logger.info("Promotion with ID [%s] created.", promo.id)
-    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+#     app.logger.info("Promotion with ID [%s] created.", promo.id)
+#     return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
 
 
 # ######################################################################
@@ -281,63 +397,63 @@ def create_promo():
 #     app.logger.info("Promo with ID [%s] delete complete.", promo_id)
 #     return "", status.HTTP_204_NO_CONTENT
 
-######################################################################
-# LIST ALL PROMOTIONS
-######################################################################
+# ######################################################################
+# # LIST ALL PROMOTIONS
+# ######################################################################
 
 
-@app.route("/promotions", methods=["GET"])
-def list_promos():
-    """Returns all of the Promos"""
-    app.logger.info("Request for promo list")
-    promotions = []
+# @app.route("/promotions", methods=["GET"])
+# def list_promos():
+#     """Returns all of the Promos"""
+#     app.logger.info("Request for promo list")
+#     promotions = []
 
-    # check whether query condition is supported
-    for key in request.args.keys():
-        if not (key in ["type", "name", "discount", "customer", "start_date", "end_date"]):
-            abort(status.HTTP_400_BAD_REQUEST,
-                  f"unsupported query condition: {key}")
+#     # check whether query condition is supported
+#     for key in request.args.keys():
+#         if not (key in ["type", "name", "discount", "customer", "start_date", "end_date"]):
+#             abort(status.HTTP_400_BAD_REQUEST,
+#                   f"unsupported query condition: {key}")
 
-    promotions = Promotion.all()
+#     promotions = Promotion.all()
 
-    query_type = request.args.get('type')
-    if query_type != None:
-        app.logger.info("type = %s", query_type)
-        promotions = filter(lambda x: (
-            query_type == str(x.type.name)), promotions)
+#     query_type = request.args.get('type')
+#     if query_type != None:
+#         app.logger.info("type = %s", query_type)
+#         promotions = filter(lambda x: (
+#             query_type == str(x.type.name)), promotions)
 
-    query_name = request.args.get('name')
-    if query_name != None:
-        app.logger.info("name contains %s", query_name)
-        promotions = filter(lambda x: (query_name in x.name), promotions)
+#     query_name = request.args.get('name')
+#     if query_name != None:
+#         app.logger.info("name contains %s", query_name)
+#         promotions = filter(lambda x: (query_name in x.name), promotions)
 
-    query_discount = request.args.get('discount')
-    if query_discount != None:
-        app.logger.info("discount = %s", query_discount)
-        promotions = filter(lambda x: (
-            int(query_discount) == x.discount), promotions)
+#     query_discount = request.args.get('discount')
+#     if query_discount != None:
+#         app.logger.info("discount = %s", query_discount)
+#         promotions = filter(lambda x: (
+#             int(query_discount) == x.discount), promotions)
 
-    query_customer = request.args.get('customer')
-    if query_customer != None:
-        app.logger.info("customer = %s", query_customer)
-        promotions = filter(lambda x: (
-            int(query_customer) == x.customer), promotions)
+#     query_customer = request.args.get('customer')
+#     if query_customer != None:
+#         app.logger.info("customer = %s", query_customer)
+#         promotions = filter(lambda x: (
+#             int(query_customer) == x.customer), promotions)
 
-    query_start_date = request.args.get('start_date')
-    if query_start_date != None:
-        app.logger.info("start_date = %s", query_start_date)
-        promotions = filter(lambda x: query_start_date ==
-                            '{:%Y-%m-%d}'.format(x.start_date), promotions)
+#     query_start_date = request.args.get('start_date')
+#     if query_start_date != None:
+#         app.logger.info("start_date = %s", query_start_date)
+#         promotions = filter(lambda x: query_start_date ==
+#                             '{:%Y-%m-%d}'.format(x.start_date), promotions)
 
-    query_end_date = request.args.get('end_date')
-    if query_end_date != None:
-        app.logger.info("end_date = %s", query_end_date)
-        promotions = filter(lambda x: query_end_date ==
-                            '{:%Y-%m-%d}'.format(x.end_date), promotions)
+#     query_end_date = request.args.get('end_date')
+#     if query_end_date != None:
+#         app.logger.info("end_date = %s", query_end_date)
+#         promotions = filter(lambda x: query_end_date ==
+#                             '{:%Y-%m-%d}'.format(x.end_date), promotions)
 
-    results = [promo.serialize() for promo in promotions]
-    app.logger.info("Returning %d promotions", len(results))
-    return jsonify(results), status.HTTP_200_OK
+#     results = [promo.serialize() for promo in promotions]
+#     app.logger.info("Returning %d promotions", len(results))
+#     return jsonify(results), status.HTTP_200_OK
 
 # ######################################################################
 # # UPDATE AN EXISTING PROMOTION
@@ -391,25 +507,25 @@ def list_promos():
 ######################################################################
 
 
-@app.route("/promotions/<promo_id>/cancel", methods=["PUT"])
-def early_cancel_promotion(promo_id):
-    """
-    Cancel a Promotion early
+# @app.route("/promotions/<promo_id>/cancel", methods=["PUT"])
+# def early_cancel_promotion(promo_id):
+#     """
+#     Cancel a Promotion early
 
-    This endpoint will set the end date of Promotion with ID `promo_id` to its start date.
-    A Promotion with equal start and end dates is semantically considered canceled.
-    """
-    app.logger.info("Request to cancel a Promotion with id: %s", promo_id)
-    # attempt to locate Promotion for early cancellation
-    promotion = Promotion.find(promo_id)
-    if not promotion:
-        abort(status.HTTP_404_NOT_FOUND,
-              f"Promotion with id '{promo_id}' was not found.")
-    # set the information
-    promotion.end_date = promotion.start_date
-    promotion.update()
-    app.logger.info("Promotion with ID [%s] has been canceled.", promotion.id)
-    return jsonify(promotion.serialize()), status.HTTP_200_OK
+#     This endpoint will set the end date of Promotion with ID `promo_id` to its start date.
+#     A Promotion with equal start and end dates is semantically considered canceled.
+#     """
+#     app.logger.info("Request to cancel a Promotion with id: %s", promo_id)
+#     # attempt to locate Promotion for early cancellation
+#     promotion = Promotion.find(promo_id)
+#     if not promotion:
+#         abort(status.HTTP_404_NOT_FOUND,
+#               f"Promotion with id '{promo_id}' was not found.")
+#     # set the information
+#     promotion.end_date = promotion.start_date
+#     promotion.update()
+#     app.logger.info("Promotion with ID [%s] has been canceled.", promotion.id)
+#     return jsonify(promotion.serialize()), status.HTTP_200_OK
 
 
 ######################################################################
